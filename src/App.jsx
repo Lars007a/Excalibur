3350f49f363a60ab1b6ac908f21174ffe96266e9
RamaMamora <rama.mamora@icloud.com>
2025-01-09 09:25:22 +0100

    tilføjelse af footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Frontpage from "./pages/frontpage/frontpage.jsx";
 import { useRoutes } from "react-router-dom";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
   let routes = useRoutes([
@@ -11,7 +12,12 @@ function App() {
     {},
   ]);
 
-  return <>{routes}</>;
+  return (
+    <>
+      <div> {routes}</div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
