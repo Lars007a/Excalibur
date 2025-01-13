796753dc830ac360be5d46132819028a48e0e8bf
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2025-01-13 23:12:36 +0100

    events content

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -7,36 +7,37 @@ const EventsHero = ({ eventsHeroImg }) => {
       style={{ backgroundImage: `url(${eventsHeroImg})` }}
     >
       <div className={styles.eventsContainer}>
-        <div className={styles.eventsSection}>
-          <h1>Arrangementer i Undallslund</h1>
+        <div className={styles.eventsContent}>
+          <div className={styles.eventsSection}>
+            <h1>Arrangementer i Undallslund</h1>
+            <p className={styles.eventsInfo}>
+              Vi spiller i forårssæsonen og efterårssæsonen. Forårssæsonen går
+              typisk fra starten af marts til sommerferien, og efterårssæsonen
+              går fra sommerferien slutter til det bliver for koldt en gang i
+              november. Vi spiller søndage i lige uger – datoer finder du
+              herunder.
+            </p>
+            <ul className={styles.eventsInfo}>
+              <li>2025:</li>
+              <li>søndag d. 16. marts</li>
+              <li>søndag d. 30. marts</li>
+              <li>søndag d. 13. april</li>
+              <li>søndag d. 27. april</li>
+              <li>søndag d. 11. maj</li>
+              <li>søndag d. 31. maj Aflyser vi.</li>
+              <li>søndag d. 15. juni</li>
+              <li>lørdag d. 28. juni (fælles afslutning)</li>
+            </ul>
+          </div>
           <div className={styles.playerImg}>
             <img src="/Warrior_Run_0.png" alt="player" />
           </div>
-          <p>
-            Vi spiller i forårssæsonen og efterårssæsonen. Forårssæsonen går
-            typisk fra starten af marts til sommerferien, og efterårssæsonen går
-            fra sommerferien slutter til det bliver for koldt en gang i
-            november. Vi spiller søndage i lige uger – datoer finder du
-            herunder.
-          </p>
-          <ul>
-            <li>2025:</li>
-            <li>søndag d. 16. marts</li>
-            <li>søndag d. 30. marts</li>
-            <li>søndag d. 13. april</li>
-            <li>søndag d. 27. april</li>
-            <li>søndag d. 11. maj</li>
-            <li>søndag d. 31. maj Aflyser vi.</li>
-            <li>søndag d. 15. juni</li>
-            <li>lørdag d. 28. juni (fælles afslutning)</li>
-          </ul>
         </div>
-
         <div>
           <ul>
             <li className={styles.eventsSectionItem}>
               <h2>Tid og sted</h2>
-              <p>
+              <p className={styles.eventsInfo}>
                 Spillere chekkes ind fra 11.00 til 11:30. Spillet begynder kl.
                 12:00, når alle er blevet sminket og klar. Mellem 11.00 og 12.00
                 er der selvfølgelig mulighed for at lege og slås med nogen af de
@@ -51,7 +52,7 @@ const EventsHero = ({ eventsHeroImg }) => {
             </li>
             <li className={styles.eventsSectionItem}>
               <h2>Sæsonkort</h2>
-              <p>
+              <p className={styles.eventsInfo}>
                 Man kan til Rollespil i Undallslund købe sæsonkort. Prisen på
                 dette er altid 100 kr. + 50 kr. per resterende spilgang i
                 sæsonen. Hvis man ikke allerede er medlem ved køb af
@@ -67,18 +68,20 @@ const EventsHero = ({ eventsHeroImg }) => {
           <ul className={styles.gridEvents}>
             <li className={styles.eventsItem}>
               <h2>Pris</h2>
-              <p>
+              <p className={styles.eventsInfo}>
                 Det koster 100 kr. at deltage til Rollespil i Undallslund og så
                 bliver du også medlem af foreningen Excalibur.
               </p>
             </li>
             <li className={styles.eventsItem}>
               <h2>Lån af sværd og kostume</h2>
-              <p>Vi har både kostumer og våben du kan låne ganske gratis.</p>
+              <p className={styles.eventsInfo}>
+                Vi har både kostumer og våben du kan låne ganske gratis.
+              </p>
             </li>
             <li className={styles.eventsItem}>
               <h2>Madpakke</h2>
-              <p>
+              <p className={styles.eventsInfo}>
                 Man skal selv sørge for madpakke, for ca. kl. 13:30 spiser vi
                 frokost. Husk også at tage noget at drikke med, da man nemt kan
                 blive tørstig af at rende rundt og slås!
@@ -86,7 +89,7 @@ const EventsHero = ({ eventsHeroImg }) => {
             </li>
             <li className={styles.eventsItem}>
               <h2>Spørgsmål</h2>
-              <p>
+              <p className={styles.eventsInfo}>
                 Hvis du har spørgsmål er du altid velkommen til at e-maile eller
                 ringe til os. Du kan finde vores kontaktinformation under
                 Kontakt.
--- src/pages/arrangements/Arrangements.jsx
+++ src/pages/arrangements/Arrangements.jsx
@@ -3,9 +3,11 @@ import eventsHeroImg from "../../assets/notSortedImgs/bagground_img.jpg";
 
 const Arrangements = () => {
   return (
-    <article>
-      <EventsHero eventsHeroImg={eventsHeroImg} />
-    </article>
+    <>
+      <section>
+        <EventsHero eventsHeroImg={eventsHeroImg} />
+      </section>
+    </>
   );
 };
 
