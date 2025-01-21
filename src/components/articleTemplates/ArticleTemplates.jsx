00f230d826401ce1763d268ac3394a59422520f2
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2025-01-21 13:08:30 +0100

    update articlePage

--- src/components/articleTemplates/ArticleTemplates.jsx
+++ src/components/articleTemplates/ArticleTemplates.jsx
@@ -18,22 +18,16 @@ export const articlesTmpl = (article) => (
         <img src={article.largeImg} alt="article image" />
       </div>
       <div className={styles.articleTextContainer}>
-        <div className={styles.articleTextContainer}>
-          <h1 className={styles.heading}>{article.heading}</h1>
-          <div className={styles.bylineContainer}>
-            <p className={styles.author}> af {article.author} |</p>
-            <p className={styles.date}>{article.date}</p>
-          </div>
-          <div className={styles.bodyTextContainer}>
-            <p className={styles.bodyTextHeadline}>
-              {article.bodyTextHeadline1}
-            </p>
-            <p className={styles.bodyText}>{article.bodyText1}</p>
-            <p className={styles.bodyTextHeadline}>
-              {article.bodyTextHeadline2}
-            </p>
-            <p className={styles.bodyText}>{article.bodyText2}</p>
-          </div>
+        <h1 className={styles.heading}>{article.heading}</h1>
+        <div className={styles.bylineContainer}>
+          <p className={styles.author}> af {article.author} |</p>
+          <p className={styles.date}>{article.date}</p>
+        </div>
+        <div className={styles.bodyTextContainer}>
+          <p className={styles.bodyTextHeadline}>{article.bodyTextHeadline1}</p>
+          <p className={styles.bodyText}>{article.bodyText1}</p>
+          <p className={styles.bodyTextHeadline}>{article.bodyTextHeadline2}</p>
+          <p className={styles.bodyText}>{article.bodyText2}</p>
         </div>
       </div>
     </div>
