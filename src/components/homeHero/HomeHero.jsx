10d58576744348173bf183e5ed737958926d1ded
Lars <laarrss@protonmail.com>
2025-01-23 10:03:37 +0100

    Knappen på forsiden skifter mellem login og logud.

--- src/components/homeHero/HomeHero.jsx
+++ src/components/homeHero/HomeHero.jsx
@@ -1,8 +1,13 @@
 import styles from "./homeHero.module.css";
 import { Link } from "react-router-dom";
 import baggroundImg from "../../assets/notSortedImgs/bagground_img.jpg";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 const HomeHero = () => {
+  const [savedUser, setSavedUser] = useLocalStorage("savedUser", {});
+
+  const handleLogout = () => setSavedUser({});
+
   return (
     <section className={styles.homeHero}>
       <img
@@ -10,9 +15,16 @@ const HomeHero = () => {
         src={baggroundImg}
         alt="Picture of dirt background"
       />
-      <Link to="/Login" className={styles.loginBtn}>
-        Login
-      </Link>
+
+      {savedUser.loggedIn == true ? (
+        <Link className={styles.loginBtn} onClick={handleLogout}>
+          Logout
+        </Link>
+      ) : (
+        <Link to="/Login" className={styles.loginBtn}>
+          Login
+        </Link>
+      )}
       <img
         className={styles.HomeHeroLogo}
         src="/logo.png"
